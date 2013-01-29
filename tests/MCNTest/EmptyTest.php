f3160678a08fce291c4057fe4be8594c11f65e53
Antoine Hedgecock <antoine@pmg.se>
2013-01-29 13:11:49 +0100

    added tests

---
+++ tests/MCNTest/EmptyTest.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * @author Antoine Hedgecock <antoine@pmg.se>
+ * @author Jonas Eriksson <jonas@pmg.se>
+ *
+ * @copyright PMG Media Group AB
+ */
+
+namespace MCNTest;
+
+class EmptyTest extends \PHPUnit_Framework_TestCase
+{
+    public function testAssertTrue()
+    {
+        $this->assertTrue(true);
+    }
+}
